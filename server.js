0b8d17de74aeff59269ecddd3f0cdcef1e1e9373
fadlurss <jhieber7@gmail.com>
2019-11-27 10:04:25 +0700

    update server.js
    penambahan konek ke database menggunakan MySQL

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var express = require('express'),
+    mysql = require('mysql'),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
     routes = require('./routes/routes_latihan');
 
 // mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/API');
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/API');
+
+// app.js
+
+
+// First you need to create a connection to the db
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+});
+
+con.connect((err) => {
+    if (err) {
+        console.log('Error connecting to Db');
+        return;
+    }
+    console.log('Connection established');
+});
+
+con.end((err) => {
+    // The connection is terminated gracefully
+    // Ensures all previously enqueued queries are still
+    // before sending a COM_QUIT packet to the MySQL server.
+});
 
 app = express(),
     app.use(bodyParser.json());
